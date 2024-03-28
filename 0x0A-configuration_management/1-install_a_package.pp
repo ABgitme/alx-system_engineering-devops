c100e0866160cdd1b433453f67825b25119efc49
ABgitme <abebekezhabesha@gmail.com>
2024-03-28 05:40:16 +0300

    1-install_a_package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 #!/usr/bin/pup
 # parameter ensures that Puppet only executes the command if Flask version 2.1.0 is not already installed
-class { 'python3': }
-package { 'flask':
+package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
