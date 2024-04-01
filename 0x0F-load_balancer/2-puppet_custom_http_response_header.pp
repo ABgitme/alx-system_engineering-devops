60be499a33d218d164293e5441408abe9aad4cf3
ABgitme <abebekezhabesha@gmail.com>
2024-04-01 09:45:48 +0300

    2-puppet_custom_http_response_header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,35 @@
+# Define a class for configuring custom HTTP response header
+class custom_http_response_header {
+
+  # Install Nginx package
+  package { 'nginx':
+    ensure => installed,
+  }
+
+  # Configure custom HTTP response header using sed
+  exec { 'add_custom_header':
+    command  => "sudo sed -i '/server_name _/a add_header X-Served-By ${::hostname};' /etc/nginx/sites-enabled/default",
+    unless   => "grep -q 'X-Served-By' /etc/nginx/sites-enabled/default",
+    provider => 'shell',
+    require  => Package['nginx'],
+    notify   => Service['nginx'],
+  }
+
+  # Enable the default site
+  file { '/etc/nginx/sites-enabled/default':
+    ensure  => 'link',
+    target  => '/etc/nginx/sites-available/default',
+    require => File['/etc/nginx/sites-available/default'],
+    notify  => Service['nginx'],
+  }
+
+  # Restart Nginx to apply changes
+  service { 'nginx':
+    ensure    => running,
+    enable    => true,
+    require => Package['nginx']
+  }
+}
+
+# Apply the custom HTTP response header configuration
+include custom_http_response_header
