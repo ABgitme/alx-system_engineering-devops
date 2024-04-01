047bccd6c39b307ad08f154e197f10312afb1641
ABgitme <abebekezhabesha@gmail.com>
2024-04-01 14:22:25 +0300

    2-puppet_custom_http_response_header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -5,22 +5,22 @@ exec {'system update':
   user     => 'root',
   provider => 'shell',
 }
-  # Install Nginx package
-  package { 'nginx':
-    ensure => 'installed',
-    require => Exec['system update']
-  }
+# Install Nginx package
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['system update']
+}
 
-  # Configure custom HTTP response header using sed
-  exec {'HTTP header':
-	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
-	provider => 'shell'
+# Configure custom HTTP response header using sed
+exec {'HTTP header':
+  command  => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+  provider => 'shell'
 }
 
 
-  # Restart Nginx to apply changes
-  service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => Package['nginx']
+# Restart Nginx to apply changes
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx']
   }
