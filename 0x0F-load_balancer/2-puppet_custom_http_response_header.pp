a44abeb650f221918739d5081b1af7d9732e5cef
ABgitme <abebekezhabesha@gmail.com>
2024-04-01 14:33:22 +0300

    2-puppet_custom_http_response_header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,16 @@
 # Define a class for configuring custom HTTP response header
 
-exec { 'update system':
+exec { 'system update':
         command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
   ensure  => 'installed',
-  require => Exec['update system']
+  require => Exec['system update'],
 }
 
 exec {'HTTP header':
-  command  => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+  command  => 'sed -i "/server_name _/a\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
   provider => 'shell'
 }
 
