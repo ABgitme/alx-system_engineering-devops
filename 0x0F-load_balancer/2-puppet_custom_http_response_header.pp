a1b7bdc7c9360c7d3f90b61f7d0712f2e6979b3e
ABgitme <abebekezhabesha@gmail.com>
2024-04-01 14:26:16 +0300

    2-puppet_custom_http_response_header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,34 +1,29 @@
 # Define a class for configuring custom HTTP response header
 
-exec {'system update':
-  command  => 'apt-get update'
-  user     => 'root',
-  provider => 'shell',
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
 }
-# Install Nginx package
+
 package { 'nginx':
   ensure  => 'installed',
-  require => Exec['system update']
+  require => Exec['update system']
 }
+
 file {'/var/www/html/index.html':
-	content => 'Hello World!'
+  content => 'Hello World!'
 }
 
 exec {'redirect_me':
-	command => 'sed -i "24i\	rewrite ^/redirect_me https://youtube.com/watch?v=QH2-TGUlwu4/ permanent;" /etc/nginx/sites-available/default',
-	provider => 'shell'
+  command  => 'sed -i "24i\	rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell'
 }
 
-# Configure custom HTTP response header using sed
 exec {'HTTP header':
   command  => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
   provider => 'shell'
 }
 
-
-# Restart Nginx to apply changes
-service { 'nginx':
+service {'nginx':
   ensure  => running,
-  enable  => true,
   require => Package['nginx']
-  }
+}
