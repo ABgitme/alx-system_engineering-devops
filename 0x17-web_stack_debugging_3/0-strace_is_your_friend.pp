02495daa839fb87b367881487395106668851570
ABgitme <132369839+ABgitme@users.noreply.github.com>
2024-06-03 20:59:17 +0300

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,5 +0,0 @@
-# automated fix for Apache is returning a 500 error.
-exec{'fix-Apache':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/:/usr/bin/'
-}
