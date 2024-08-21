a26c936dfa6a34b7abc6a4d523c8d630a9631916
ABgitme <abebekezhabesha@gmail.com>
2024-08-21 12:57:28 +0100

    web_stack_debugging_4

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,6 @@
+
+# Enable the user holberton to login and open files without error.
+exec { 'increase-file-limits-for-holberton-user':
+  command => 'sed -i "/^holberton hard/s/5/50000/; /^holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => ['/usr/local/bin', '/bin'],
+}
